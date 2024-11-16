5034c9341a0bccb0dad18e3d290ddd30b31c777c
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-11-16 23:22:24 +0600

    add the discuss section and post section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-const loadCard = async (searchText) => {
+const loadCard = async (searchText =2) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`)
     const data = await res.json()
     const cards = data.posts
     // console.log(cards)
     displayCard(cards);
 };
-
 const displayCard = (cards) => {
     // console.log(cards)
-    
     const cardContainer = document.getElementById("card-container")
+    cardContainer.textContent = '';
     cards.forEach(card => {
         console.log(card)
         // 2- create a div
@@ -46,19 +45,80 @@ const displayCard = (cards) => {
         let score = 0;
         const buttonClick = document.getElementById("button-click")
         const scoreDisplay = document.getElementById("score")
-        buttonClick.addEventListener('click', function(){
+        buttonClick.addEventListener('click', function(cards){
         score++;
         scoreDisplay.innerText = score;
+        console.log(cards)
         })
+        
     });
+    toggoleLoadingSpinner(false);
 }
 
 const handleSearch = () => {
     // console.log('conected handleSearch')
+
     const searchField = document.getElementById("search-field");
+    toggoleLoadingSpinner(true);
     const searchText = searchField.value;
     console.log(searchText)
     loadCard(searchText)
+    // loadPhone(searchText, isShowAll);
+
 }
 
-loadCard()
+const toggoleLoadingSpinner = (isLoding) =>{
+    const loadingSpinner = document.getElementById("loading-spinner");
+    // loadingSpinner.classList.remove('hidden')
+    if(isLoding){
+        loadingSpinner.classList.remove('hidden')
+    }
+    else{
+        loadingSpinner.classList.add('hidden')
+    }
+}
+
+loadCard();
+
+
+const latestPost = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`)
+    const data = await res.json();
+    // console.log(data);
+    displayPost(data);
+}
+
+const displayPost = (data) =>{
+    const postContainer = document.getElementById('post-container');
+    data.forEach(dt => {
+        // console.log(dt)
+        const postedCard = document.createElement('div');
+        postedCard.classList = 'rounded w-full border gap-16  rounded-2xl';
+        postedCard.innerHTML = `
+            <figure class="px-10 pt-10">
+              <img
+                src="${dt.cover_image}"
+                alt=""
+                class="rounded-xl" />
+            </figure>
+            <div class="card-body">
+              <p class="flex"><img src="images/Frame (2).png" alt=""> ${dt?.author?.posted_date || 'No publish date'}</p>
+              <h2 class="text-2xl roboto-bold">${dt.title}</h2>
+              <p class="text-[#03030381]">${dt.description}</p>
+              <div class="flex">
+               <div class="avatar">
+                  <div class="w-12 h-12 rounded-full mr-5">
+                    <img src="${dt.profile_image}" />
+              </div>
+            </div>
+            <div>
+            <p class="roboto-bold">${dt.author.name}</p>
+            <p>${dt?.author?.designation || 'unKown'}</p>
+            </div>
+            </div>
+        `;
+        postContainer.appendChild(postedCard);
+    });
+}
+
+latestPost()
